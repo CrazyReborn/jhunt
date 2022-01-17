0673d39a2bb962d4475748285d999da2f1729682
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-01-17 12:10:11 +0100

    added cors and dotenv. connected the app to mongodb.

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiendTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
