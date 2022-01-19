2e1435dfb550faeb22da98cdd3e9220f487234de
Vladislav Kuznetsov <vladkuzodin@gmail.com>
2022-01-19 13:29:17 +0100

    added put method and edited get method for application/:id endpoint.

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -39,7 +39,7 @@ exports.applications_post = [
     if (!errors.isEmpty()) {
       res.json({ err: errors });
     } else {
-      const post = new Application({
+      const application = new Application({
         user: req.body.userId,
         company_name: req.body.companyName,
         position: req.body.position,
@@ -58,7 +58,7 @@ exports.applications_post = [
         if (err) {
           res.json({ err });
         } else {
-          post.save((savingErr) => {
+          application.save((savingErr) => {
             if (savingErr) {
               res.json({ err: savingErr });
             } else {
@@ -71,8 +71,58 @@ exports.applications_post = [
   },
 ];
 
-exports.application_get = (req, res) => {
-  Application.findById(req.params.id)
-    .then((application) => res.json(application))
-    .catch((err) => res.json({ err }));
-};
+exports.application_get = [
+  verifyToken,
+  (req, res) => { // Add jwt.verify function
+    Application.findById(req.params.id)
+      .then((application) => res.json(application))
+      .catch((err) => res.json({ err }));
+  },
+];
+
+exports.application_put = [
+  body('companyName', 'Company name should not be empty').trim().isLength({ min: 1 }).escape(),
+  body('position', 'Position should not be empty').trim().isLength({ min: 1 }).escape(),
+  body('salary', 'Salary should not be empty').trim().isLength({ min: 1 }).escape(),
+  body('location', 'Location should not be empty').trim().isLength({ min: 1 }).escape(),
+  body('aggregator', 'Aggregator field should not be empty').trim().isLength({ min: 1 }).escape(),
+  body('link', 'Link field should not be empty').trim().isLength({ min: 1 }).escape(),
+  verifyToken,
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.json({ err: errors });
+    } else {
+      const application = new Application({
+        _id: req.params.id,
+        user: req.body.userId,
+        company_name: req.body.companyName,
+        position: req.body.position,
+        salary: req.body.salary,
+        status: req.body.status,
+        location: req.body.location,
+        aggregator: req.body.aggregator,
+        found_on: req.body.foundOn,
+        cv_sent_on: req.body.cvSentOn,
+        cv_path: req.body.cvPath,
+        job_link: req.body.link,
+        answer_received: req.body.answerReceived,
+        qualifications_met: req.body.qualifications,
+      });
+      jwt.verify(req.token, 'secretKey', (err) => {
+        if (err) {
+          res.json({ err });
+        } else {
+          Application.findByIdAndUpdate(req.params.id, application, (err) => {
+            if (err) {
+              res.json({ err });
+            } else {
+              res.send('succsess');
+            }
+          });
+        }
+      });
+    }
+  },
+];
